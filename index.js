fc235bb240a7bbd2ac12aa42d3a8b18e157c36a1
zhongshuai <zhongshuai@megvii.com>
2019-05-26 17:37:28 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+/*
+ * @Description: 
+ * @Author: zhongshuai
+ * @Date: 2019-05-26 16:24:21
+ * @LastEditors: zhongshuai
+ * @LastEditTime: 2019-05-26 16:50:30
+ */
+module.exports =  require('./lib/getSql');
